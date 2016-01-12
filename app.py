ae69d56904a01cc7e89a444c425edbb28c0145cd
c.thoens <christoph.thoens@gmail.com>
2016-01-12 10:57:54 +0100

    Now supports multiple environments

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-import connexion
 import logging
+import connexion
 
-from connexion import NoContent
+import os
+if os.environ.get('ENVIRONMENT', 'DEV') == 'PROD':
+    from config import prod as config  # config/prod.py
+else:
+    from config import dev as config  # config/dev.py
 
-#path = "/sys/bus/w1/devices/{sensor_id}/w1_slave"
-path = "testdata/{sensor_id}/w1_slave"
-sensor_ids = ["28-0315a47057ff", "28-0415a40a21ff", "28-0415a463d4ff"]
 
 def read_temperature(sensor_id):
     # Open the file that we viewed earlier so that python can see what is in it. Replace the serial number as before.
-    tfile = open(path.format(sensor_id=sensor_id))
+    tfile = open(config.path.format(sensor_id=sensor_id))
     # Read all of the text in the file.
     text = tfile.read()
     # Close the file now that the text has been read.
@@ -26,7 +27,7 @@ def read_temperature(sensor_id):
 
 
 def get_sensors(limit=100):
-    return [{"id": sensor_id, "temperature": read_temperature(sensor_id)} for sensor_id in sensor_ids][:limit]
+    return [{"id": sensor_id, "temperature": read_temperature(sensor_id)} for sensor_id in config.sensor_ids][:limit]
 
 
 def get_sensor(sensor_id):
@@ -47,4 +48,4 @@ application = app.app
 
 if __name__ == '__main__':
     # run our standalone gevent server
-    app.run(port=8080, server='gevent')
+    app.run(port=config.port, server='gevent')
